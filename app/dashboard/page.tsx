abc54cb6cf98121649a0bdd363e749a5e100bd5f
Sanyam Thakur <sanyamthakurr2003@gmail.com>
2025-10-02 12:24:10 +0530

    Added the sidebar and dashboard header

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,28 +1,11 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
+import PageWrapper from "@/components/page-wrapper";
 import React from "react";
 
 const Page = () => {
-  const router = useRouter();
-
-  const handleSignOut = async () => {
-    await authClient.signOut();
-    router.push("/");
-  };
-
   return (
-    <div className="flex items-center justify-center  h-screen">
-      <Button
-        onClick={handleSignOut}
-        variant={"outline"}
-        className="cursor-pointer"
-      >
-        Sign Out
-      </Button>
-    </div>
+    <PageWrapper breadcrumbs={[{ label: "Dashboard", href: "/dashboarod" }]}>
+      Homepage
+    </PageWrapper>
   );
 };
 
---
+++ components/page-wrapper.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import React from "react";
+import { SidebarTrigger } from "./ui/sidebar";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { authClient } from "@/lib/auth-client";
+import { ModeToggle } from "./mode-toggle";
+
+interface PageWrapperProps {
+  children: React.ReactNode;
+  breadcrumbs: {
+    label: string;
+    href: string;
+  }[];
+}
+
+const PageWrapper = ({ children, breadcrumbs }: PageWrapperProps) => {
+  const router = useRouter();
+  const handleSignOut = async () => {
+    await authClient.signOut();
+    router.push("/");
+  };
+  return (
+    <div>
+      <div className="flex items-center justify-between border-b">
+        <div className="flex items-center gap-3">
+          <SidebarTrigger />
+          <Breadcrumb>
+            <BreadcrumbList>
+              {breadcrumbs.map((breadcrumb) => (
+                <React.Fragment key={breadcrumb.label}>
+                  <BreadcrumbItem>
+                    <BreadcrumbLink href={breadcrumb.href}>
+                      {breadcrumb.label}
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator />
+                </React.Fragment>
+              ))}
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+        <div className="flex items-center gap-4 p-4">
+          <ModeToggle />
+          <Button onClick={handleSignOut} type="button">
+            Sign Out
+          </Button>
+        </div>
+      </div>
+      <div>{children}</div>
+    </div>
+  );
+};
+
+export default PageWrapper;
